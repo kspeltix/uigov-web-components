4ecef381bbd13153718090a525f15ba6401ab8d0
kspeltix <kris.speltincx@vlaanderen.be>
2023-05-16 12:14:12 +0200

    feat: UIG-2466 - bamboo build

--- resources/ci/perform-all-unit-tests.sh
+++ resources/ci/perform-all-unit-tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo 'RUNNING SCRIPT: perform-all-unit-tests.sh'
+echo "npm ci"
+npm ci
+echo "run all unit tests"
 npx nx run-many --all --target=test --parallel --maxParallel=4 --skip-nx-cache
--- resources/ci/perform-all-web-tests.sh
+++ resources/ci/perform-all-web-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 echo 'RUNNING SCRIPT: perform-all-web-tests.sh'
+echo "npm ci"
+npm ci
+echo "run all web tests"
 npm run build:all
 npx nx wct map
