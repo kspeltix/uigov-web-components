ac71633b55bb8d9697c9f51b9de84f206bb37851
kspeltix <kris.speltincx@vlaanderen.be>
2023-05-17 14:39:07 +0200

    feat: UIG-2466 - bamboo build

---
+++ resources/ci/e2e-test-storybook-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# exit bij een fout
+set -e
+
+echo 'RUNNING SCRIPT: e2e-test-storybook-1.sh'
+cd uigov-web-components
+echo "npm ci"
+npm ci
+echo "run the e2e tests"
+npm run storybook:ci-test-parallel-1
--- resources/ci/e2e-test-storybook-2.sh
+++ resources/ci/e2e-test-storybook-2.sh
@@ -3,9 +3,9 @@
 # exit bij een fout
 set -e
 
-echo 'RUNNING SCRIPT: e2e-test-storybook.sh'
+echo 'RUNNING SCRIPT: e2e-test-storybook-2.sh'
 cd uigov-web-components
 echo "npm ci"
 npm ci
 echo "run the e2e tests"
-npm run storybook:ci-test-parallel-1
+npm run storybook:ci-test-parallel-2
